b06ac2eb53b3a617e9614af7d1420a0213bb1b31
Svngoku <marymarty104@gmail.com>
2019-02-22 20:37:20 +0100

    Mise à jour file generate-word-F2.js

--- tips/generate-word-F2.js
+++ tips/generate-word-F2.js
@@ -1,11 +1,11 @@
 /* en : Every time you press F2 , u generate the spy emoji 🕵️  */
-/* fr : Toute les fois que je vous appuyer F2 , vous generer l'emoji dans le champs pointé   */
+/* fr : Toute les fois que vous appuyer F2 , vous generer l'emoji dans le champs pointé   */
 
 
 let textarea = document.querySelector('textarea')
 textarea.addEventListener("keydown", event => {
 	if(event.keycode === 113) {
-	 replaceSelection(textarea, "🕵️ ");
+	 replaceSelection(textarea, "🕵️");
 	 event.preventDefault();
 	}
 });
