008829133c58a6dcfb6239171773fd2c104ce318
Svngoku <marymarty104@gmail.com>
2019-02-24 21:27:19 +0100

    update closure-loop.js

--- tips/closure-loop.js
+++ tips/closure-loop.js
@@ -1,5 +1,5 @@
 /*
- * Do a closure loop with IIFE ( function scoping )
+ * Do a closure loop with IIFE ( Immediately-Invoked Function Expression )
  */
 for (var x = 1, i = 0; i <= 32; i+= 1) {
 	(function(i){
