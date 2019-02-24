95e3a74942dca8b37b847da3c00347b301000eb0
Svngoku <marymarty104@gmail.com>
2019-02-24 21:24:34 +0100

    closure loop &

---
+++ tips/closure-loop.js
@@ -0,0 +1,11 @@
+/*
+ * Do a closure loop with IIFE ( function scoping )
+ */
+for (var x = 1, i = 0; i <= 32; i+= 1) {
+	(function(i){
+	   setTimeout(function(){
+	    	console.log('2^%d = %d', i, x);
+	    	x += x;
+	   }, 1000);
+	})(i);
+}
