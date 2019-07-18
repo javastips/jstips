63eb1216c037c508bf6fa0489d41cdadc0b57dd1
Svngoku <marymarty104@gmail.com>
2019-07-18 23:34:23 +0200

    maths lib

---
+++ library/math.js
@@ -0,0 +1,12 @@
+const math = {};
+
+// addition of numbers
+math.additon = function(x,y) {
+    return x + y;
+}
+
+// multiplication of numbers 
+math.multiplication = function(x,y) {
+    return x * y;
+}
+
