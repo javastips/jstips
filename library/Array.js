4b77e951b615f6f7935292ceaba3c99d11bacc2a
Svngoku <marymarty104@gmail.com>
2019-08-22 19:57:15 +0200

    adding cool things

--- library/Array.js
+++ library/Array.js
@@ -7,9 +7,26 @@ Array.dim = function(dimension, initial) {
     return a;
 }
 
+// return the length of an element an params
+Array.LengthOf = function(element) {
+    if(typeof element === "object")  return element.length;
+    else return new Error('Argument must be an array')
+};
 
-const { dim  } = Array;
+Array.withoutDuplicate = function(array) {
+    var newArray = new Set();
+    array.forEach((element, index) => newArray.add(element))
+    return newArray;
+}
+
+
+// Array.findSum = function() {
+
+// }
+
+
+const { dim, LengthOf } = Array;
 
 module.exports = {
-    dim
+    dim , LengthOf
 };
